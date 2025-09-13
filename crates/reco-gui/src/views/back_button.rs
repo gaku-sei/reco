b83080bcb7dbdbd96c5f69bea87cb046989ffc0d
COMBRIAT Kévin <japan.combriat.kevin@gmail.com>
2025-09-13 11:48:33 +0900

    chore: Update all dependencies and rust to v1.89

--- crates/reco-gui/src/views/back_button.rs
+++ crates/reco-gui/src/views/back_button.rs
@@ -9,10 +9,10 @@ pub fn back_button(path: &Path, tx: &UiInboxSender<Message>, ui: &mut egui::Ui) 
     if let Some(parent) = path.parent() {
         let parent_path_str = parent.to_string_lossy();
 
-        if ui.button("back").clicked() {
-            if let Err(err) = tx.send(Message::Navigate(parent_path_str.into_owned())) {
-                error!("send error: {err:?}");
-            }
+        if ui.button("back").clicked()
+            && let Err(err) = tx.send(Message::Navigate(parent_path_str.into_owned()))
+        {
+            error!("send error: {err:?}");
         }
     }
 }
